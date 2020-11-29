b799563b838d07ffc57a9fd07be7016b9a3539e0
github username <jenniferagrenier@gmail.com>
2020-11-29 13:27:07 -0700

    adjusted seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,5 @@ customer1 = Customer.create(
     email: "jenniferagrenier@gmail.com",
     vehicle_type: "bicycle",
     vehicle_name: "Blucifer",
-    vehicle_length: 15
-)
-customer2 = Customer.create(
-    first_name: "Brett",
-    last_name: "Friedman", 
-    email: "BFAdventure@gmail.com",
-    vehicle_type: "sailboat",
-    vehicle_name: "Matilda",
-    vehicle_length: 30
+    vehicle_length: 5
 )
